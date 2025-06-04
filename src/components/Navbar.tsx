344d141fe674db04ee474e2db4d6f2b9705ed138
UzarrAhm3d <uzair.ezra7@gmail.com>
2025-06-04 16:48:29 +0500

    Built a ColorMode Toggle

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,24 @@
-import { HStack, Image, Text } from "@chakra-ui/react"
+import { Button, ClientOnly, HStack, IconButton, Image, Skeleton, Text } from "@chakra-ui/react"
+import { useColorMode } from "./ui/color-mode";
+import { LuMoon, LuSun } from "react-icons/lu";
 import logo from '../assets/logo.webp';
 
 const Navbar = () => {
+
+  const { toggleColorMode, colorMode } = useColorMode()
+
   return (
+
     <HStack>
         <Image src={logo} boxSize={"60px"}/>
+        
         <Text>Navbar</Text>
+
+        <ClientOnly fallback={<Skeleton boxSize="8" />}>
+          <IconButton onClick={toggleColorMode} variant="outline" size="sm">
+            {colorMode === "light" ? <LuSun /> : <LuMoon />}
+          </IconButton>
+        </ClientOnly> 
     </HStack>
   )
 }
